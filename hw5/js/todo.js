2d2f71ea500b76085d587042429c3c0a093aa468
Yixin Zheng <yixinzh@umich.edu>
2021-02-26 03:08:13 -0500

    hw5

---
+++ hw5/js/todo.js
@@ -0,0 +1,93 @@
+
+const timeLeft=document.getElementById('time_left');
+const currentTask=document.getElementById('current_task');
+const clock=document.getElementById('clock');
+
+function addTask(description){
+    const taskList=document.getElementById('task_list');
+    
+    const newTask=document.createElement('li');
+    const HourInput=document.createElement('input');
+    const descriptionText=document.createElement('p');
+    const removeButton=document.createElement('i')
+
+    descriptionText.textContent=description
+    descriptionText.setAttribute('style','display:inline-block')
+    descriptionText.setAttribute('id',description)
+
+    HourInput.setAttribute('type','number');
+    HourInput.setAttribute('step','0.5');
+    HourInput.setAttribute('class','col-sm-3 hour');
+    HourInput.setAttribute('value','0');
+
+    removeButton.setAttribute('class','fas fa-trash')
+    removeButton.addEventListener('click',()=>{
+        newTask.remove()
+    })
+
+    newTask.setAttribute('style','margin-bottom:10px col-sm-1')
+    newTask.append(descriptionText);
+    newTask.append(HourInput);
+    newTask.append(removeButton);
+    taskList.append(newTask);
+    newTask.addEventListener('click',function(){taskToCurrent(description,newTask)});
+    taskToCurrent(description,newTask)   
+}
+
+function taskToCurrent(description,newTask){
+    currentTask.textContent=description;
+    
+    const hours=newTask.children[1].value;
+    clock.textContent=moment().add(hours,'hour').format('HH:mm:ss A');
+    timeLeft.textContent=moment.utc(hours*60*60*1000).format('HH:mm:ss')
+    updateCurrentTask();
+
+    
+};
+
+
+addTask('Exercise')
+addTask('Cooking')
+
+
+
+
+
+const addButton=document.getElementById('add_button');
+addButton.addEventListener('click',() =>{
+    const descriptionInput = document.getElementById('task_input');
+    description=descriptionInput.value;
+    addTask(description)
+})
+
+
+
+function updateCurrentTask(){
+    // const clock = document.getElementById('clock');
+    var diff= moment(clock.textContent,'HH:mm:ss A').diff(moment());
+    if (diff>0){
+        var update=setInterval(updateTimeLeft,1000);}    
+};
+
+function updateTimeLeft(){
+    
+    // const clock = document.getElementById('clock');
+    var diff= moment(clock.textContent,'HH:mm:ss A').diff(moment());
+    if (diff<=0){
+        timeLeft.textContent=moment.utc(0).format('HH:mm:ss');
+    }else{
+    timeLeft.textContent=moment.utc(diff).format('HH:mm:ss');
+}}
+
+
+const finishedButton=document.getElementById('finished')
+finishedButton.addEventListener('click', finishTask)
+
+
+function finishTask(){
+    const descriptionText= document.getElementById(currentTask.textContent);
+    descriptionText.setAttribute('style','display:inline-block;text-decoration:line-through;color:black;')
+    clock.textContent=moment().format('HH:mm:ss:A');
+    timeLeft.textContent=moment.utc(0).format('HH:mm:ss');
+    
+}
