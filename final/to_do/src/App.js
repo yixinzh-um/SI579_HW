9d1e556e3d4aa63d8f355366f1e138d8ba195bab
Yixin Zheng <yixinzh@umich.edu>
2021-04-19 02:24:15 -0400

    final project

---
+++ final/to_do/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import TodoListDisplay from './todoListDisplay'
+
+function App() {
+  return(
+    <main>
+      <TodoListDisplay />
+      
+      
+    </main>
+
+  )
+  
+};
+
+
+export default App;
+
+
